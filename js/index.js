e9d18b95baa5141da5eef01fbb13963cb2a91581
Priyanka <priyanka.sandeep.navale@gmail.com>
2022-01-28 06:22:07 +1100

    adding cantener

--- js/index.js
+++ js/index.js
@@ -11,6 +11,21 @@ form.addEventListener("submit", (event) => {
   let validationFail = 0;
 
   event.preventDefault();
+  
+  // Call this to clear all the form fields after the submission
+  const clearFormFields = () => {
+    validateName.value = "";
+    validateDescription.value = "";
+    validateAssignedTo.value = "";
+    validateStatus.value = "In Progress";
+    validateDueDate.value = "";
+    validateName.classList.remove("is-valid");
+    validateDescription.classList.remove("is-valid");
+    validateAssignedTo.classList.remove("is-valid");
+    validateStatus.classList.remove("is-valid");
+    validateDueDate.classList.remove("is-valid");
+  };
+
 
   console.log("Task Name :" + validateName.value.length);
   console.log("Task Description :" + validateDescription.value.length);
@@ -28,8 +43,8 @@ form.addEventListener("submit", (event) => {
     validationFail++;
   }
 
-  // Form validation for Task Description Field min length 5
-  if (validateDescription.value.length > 5) {
+  // Form validation for Task Description Field min length 8
+  if (validateDescription.value.length > 8) {
     validateDescription.classList.add("is-valid");
     validateDescription.classList.remove("is-invalid");
   } else {
@@ -73,5 +88,36 @@ form.addEventListener("submit", (event) => {
   if (validationFail > 0) {
     validationFail = 0;
     return;
+  } else {
+    // Push the valid input into our tasks array
+    taskManager.addTask(
+      validateName.value,
+      validateDescription.value,
+      validateAssignedTo.value,
+      validateDueDate.value,
+      validateStatus.value
+    );
+    clearFormFields();
+    taskManager.render();
+  }
+});
+const taskList = document.querySelector("#task-list");
+// Add an 'onclick' event listener to the Tasks List
+taskList.addEventListener("click", (event) => {
+  // Check if a "Mark As Done" button was clicked
+  if (event.target.classList.contains("done-button")) {
+    // Get the correct parent Task, yours might be slightly different
+    // Use console.log(event.target.parentElement) to see
+    const parentTask =
+      event.target.parentElement.parentElement.parentElement.parentElement;
+    // Get the taskId of the parent Task and turn it into a number.
+    const taskId = Number(parentTask.dataset.taskId);
+    // Get the task from the TaskManager using the taskId
+    const task = taskManager.getTaskById(taskId);
+    // Update the task status to 'DONE'
+    task.status = "Done";
+
+    // Render the tasks
+    taskManager.render();
   }
 });
